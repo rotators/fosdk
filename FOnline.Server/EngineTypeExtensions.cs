6cdb9707a0564221087cd360699f0f4e696e733f
bprzygoda <bprzygoda@gmail.com>
2013-07-22 13:37:22 +0200

    Helpers for testing situations where either instance of managed wrapper can be null, or underlying engine structure has been invalidated

---
+++ FOnline.Server/EngineTypeExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace FOnline
+{
+    public static class EngineTypeExtensions
+    {
+        // following extensions allow cleaner tests
+        // in case when you want to check for either null instance
+        // or instance that points to cleared engine structure
+        public static bool IsNullOrNotValid(this Critter cr)
+        {
+            return cr == null || cr.IsNotValid;
+        }
+        public static bool IsNullOrNotValid(this Item item)
+        {
+            return item == null || item.IsNotValid;
+        }
+        public static bool IsNullOrNotValid(this Map map)
+        {
+            return map == null || map.IsNotValid; 
+        }
+        public static bool IsNullOrNotValid(this Location loc)
+        {
+            return loc == null || loc.IsNotValid;
+        }
+    }
+}
+
