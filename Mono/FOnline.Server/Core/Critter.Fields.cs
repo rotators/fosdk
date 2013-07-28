1bc911ac33d393bba2dcb7bf8ee243124222fd7b
bprzygoda <bprzygoda@gmail.com>
2013-07-28 16:00:12 +0200

    Scrapped offsets for Critter fields

--- Mono/FOnline.Server/Core/Critter.Fields.cs
+++ Mono/FOnline.Server/Core/Critter.Fields.cs
@@ -1,50 +1,92 @@
 using System;
+using System.Runtime.CompilerServices;
+
 namespace FOnline
 {
     public partial class Critter
     {
-        public virtual UInt32 Id { get { return NativeFields.GetUInt32(thisptr, offsetId); }}
-        public virtual UInt32 CrType { get { return NativeFields.GetUInt32(thisptr, offsetCrType); }}
-        public virtual UInt16 HexX { get { return NativeFields.GetUInt16(thisptr, offsetHexX); }}
-        public virtual UInt16 HexY { get { return NativeFields.GetUInt16(thisptr, offsetHexY); }}
-        public virtual UInt16 WorldX { get { return NativeFields.GetUInt16(thisptr, offsetWorldX); }}
-        public virtual UInt16 WorldY { get { return NativeFields.GetUInt16(thisptr, offsetWorldY); }}
-        public virtual Byte Dir { get { return NativeFields.GetByte(thisptr, offsetDir); }}
-        public virtual Cond Cond { get { return (Cond)NativeFields.GetByte(thisptr, offsetCond); }}
-        public virtual UInt32 Anim1Life { get { return NativeFields.GetUInt32(thisptr, offsetAnim1Life); }}
-        public virtual UInt32 Anim1Knockout { get { return NativeFields.GetUInt32(thisptr, offsetAnim1Knockout); }}
-        public virtual UInt32 Anim1Dead { get { return NativeFields.GetUInt32(thisptr, offsetAnim1Dead); }}
-        public virtual UInt32 Anim2Life { get { return NativeFields.GetUInt32(thisptr, offsetAnim2Life); }}
-        public virtual UInt32 Anim2Knockout { get { return NativeFields.GetUInt32(thisptr, offsetAnim2Knockout); }}
-        public virtual UInt32 Anim2Dead { get { return NativeFields.GetUInt32(thisptr, offsetAnim2Dead); }}
-        public virtual UInt32 Flags { get { return NativeFields.GetUInt32(thisptr, offsetFlags); }}
-        public virtual UInt32 ShowCritterDist1 { get { return NativeFields.GetUInt32(thisptr, offsetShowCritterDist1); } set { NativeFields.SetUInt32(thisptr, offsetShowCritterDist1, value); }}
-        public virtual UInt32 ShowCritterDist2 { get { return NativeFields.GetUInt32(thisptr, offsetShowCritterDist2); } set { NativeFields.SetUInt32(thisptr, offsetShowCritterDist2, value); }}
-        public virtual UInt32 ShowCritterDist3 { get { return NativeFields.GetUInt32(thisptr, offsetShowCritterDist3); } set { NativeFields.SetUInt32(thisptr, offsetShowCritterDist3, value); }}
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetId(IntPtr thisptr);
+        public virtual UInt32 Id { get { return GetId (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetCrType(IntPtr thisptr);
+        public virtual UInt32 CrType { get { return GetCrType (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetHexX(IntPtr thisptr);
+        public virtual UInt16 HexX { get { return GetHexX (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetHexY(IntPtr thisptr);
+        public virtual UInt16 HexY { get { return GetHexY (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetWorldX(IntPtr thisptr);
+        public virtual UInt16 WorldX { get { return GetWorldX (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetWorldY(IntPtr thisptr);
+        public virtual UInt16 WorldY { get { return GetWorldY (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static char GetDir(IntPtr thisptr);
+        public virtual Direction Dir { get { return (Direction)GetDir (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static char GetCond(IntPtr thisptr);
+        public virtual Cond Cond { get { return (Cond)GetCond (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim1Life(IntPtr thisptr);
+        public virtual UInt32 Anim1Life { get { return GetAnim1Life (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim1Knockout(IntPtr thisptr);
+        public virtual UInt32 Anim1Knockout { get { return GetAnim1Knockout (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim1Dead(IntPtr thisptr);
+        public virtual UInt32 Anim1Dead { get { return GetAnim1Dead (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim2Life(IntPtr thisptr);
+        public virtual UInt32 Anim2Life { get { return GetAnim2Life (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim2Knockout(IntPtr thisptr);
+        public virtual UInt32 Anim2Knockout { get { return GetAnim2Knockout (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetAnim2Dead(IntPtr thisptr);
+        public virtual UInt32 Anim2Dead { get { return GetAnim2Dead (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetFlags(IntPtr thisptr);
+        public virtual UInt32 Flags { get { return GetFlags (thisptr); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetShowCritterDist1(IntPtr thisptr);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static void SetShowCritterDist1(IntPtr thisptr, uint val);
+        public virtual UInt32 ShowCritterDist1 { get { return GetShowCritterDist1 (thisptr); } set { SetShowCritterDist1 (thisptr, value); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetShowCritterDist2(IntPtr thisptr);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static void SetShowCritterDist2(IntPtr thisptr, uint val);
+        public virtual UInt32 ShowCritterDist2 { get { return GetShowCritterDist2 (thisptr); } set { SetShowCritterDist2 (thisptr, value); }}
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static uint GetShowCritterDist3(IntPtr thisptr);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static void SetShowCritterDist3(IntPtr thisptr, uint val);
+        public virtual UInt32 ShowCritterDist3 { get { return GetShowCritterDist3 (thisptr); } set { SetShowCritterDist3 (thisptr, value); }}
+
         //public virtual Boolean IsRuning { get { return NativeFields.GetBoolean(thisptr, 16); } set { NativeFields.SetBoolean(thisptr, offsetIsNotValid, value); }}
-        public virtual Boolean IsNotValid { get { return NativeFields.GetBoolean(thisptr, 20); }}
-        
-#pragma warning disable 649
-		static int offsetId;
-		static int offsetCrType;
-		static int offsetHexX;
-		static int offsetHexY;
-		static int offsetWorldX;
-		static int offsetWorldY;
-		static int offsetDir;
-		static int offsetCond;
-		static int offsetAnim1Life;
-		static int offsetAnim1Knockout;
-		static int offsetAnim1Dead;
-		static int offsetAnim2Life;
-		static int offsetAnim2Knockout;
-		static int offsetAnim2Dead;
-		static int offsetFlags;
-		static int offsetShowCritterDist1;
-		static int offsetShowCritterDist2;
-		static int offsetShowCritterDist3;
-		//static int offsetIsRunning;
-		static int offsetIsNotValid;	
-#pragma warning restore 649
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static bool GetIsNotValid(IntPtr thisptr);
+        public virtual Boolean IsNotValid { get { return GetIsNotValid (thisptr); }}
     }
 }
