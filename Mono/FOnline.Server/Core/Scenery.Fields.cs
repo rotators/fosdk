88dbd3211e488ac94b793600516e17498edf90db
bprzygoda <bprzygoda@gmail.com>
2013-07-29 05:51:38 +0200

    Scenery fields based on native getters/setters

--- Mono/FOnline.Server/Core/Scenery.Fields.cs
+++ Mono/FOnline.Server/Core/Scenery.Fields.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Runtime.CompilerServices;
+
 namespace FOnline
 {
     public partial class Scenery
     {
-        public virtual UInt16 ProtoId { get { return NativeFields.GetUInt16(thisptr, offsetProtoId); }}
-        public virtual UInt16 HexX { get { return NativeFields.GetUInt16(thisptr, offsetHexX); }}
-        public virtual UInt16 HexY { get { return NativeFields.GetUInt16(thisptr, offsetHexY); }}
+        public virtual UInt16 ProtoId { get { return GetProtoId (thisptr); }}
+        public virtual UInt16 HexX { get { return GetHexX (thisptr); }}
+        public virtual UInt16 HexY { get { return GetHexY (thisptr); }}
 
-#pragma warning disable 649
-		static int offsetProtoId;
-		static int offsetHexX;
-		static int offsetHexY;
-#pragma warning restore 649
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetProtoId(IntPtr ptr);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetHexX(IntPtr ptr);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static ushort GetHexY(IntPtr ptr);
 	}
 }
